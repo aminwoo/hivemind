3d358c52b4820392d57649902e5c3ac67ec0f1f5
cyrobw <ben.woo.careers@gmail.com>
2023-03-03 09:39:19 +1100

    Use trajectory to backpropogate

--- engine/main.cpp
+++ engine/main.cpp
@@ -32,11 +32,11 @@ int main() {
     
     Bugboard board; 
 
-    board.set("r1bq1b1r/ppp1k1pp/3npp2/4N1B1/3Q4/2N2N2/PPP2KPP/R6R[NQqbnPPP] w | r1b1k2r/ppp2ppp/2p1p3/6B1/B2nn3/2P1P3/P4PPP/R1B1K2R[pPP] b kq"); 
+    /*board.set("r1bq1b1r/ppp1k1pp/3npp2/4N1B1/3Q4/2N2N2/PPP2KPP/R6R[NQqbnPPP] w | r1b1k2r/ppp2ppp/2p1p3/6B1/B2nn3/2P1P3/P4PPP/R1B1K2R[pPP] b kq"); 
     board.set_time(0, 1800); 
     board.set_time(1, 1700); 
     board.set_time(2, 1700);
-    board.set_time(3, 1800);
+    board.set_time(3, 1800);*/
 
     Agent agent;
     agent.set_is_running(true);
