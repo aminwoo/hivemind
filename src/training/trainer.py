3fc26ab1143e9c334d6e81372fca809297a57def
aminwoo <bwoo2718@gmail.com>
2024-04-20 17:18:04 +1000

    Update training script for human data

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 from typing import Any
@@ -10,11 +11,19 @@ import optax
 import orbax
 import chex
 import wandb
+import pgx
+import orbax.checkpoint as ocp
 
 from flax.training import orbax_utils
 from flax.training.train_state import TrainState
 from flax import linen as nn
 from flax.training import train_state
+
+
+def extract_params(state: TrainState): 
+    if isinstance(state, dict):
+        return {'params': state['params'], 'batch_stats': state['batch_stats']}
+    return {'params': state.params, 'batch_stats': state.batch_stats}
 
 
 class TrainState(train_state.TrainState):
@@ -25,21 +34,19 @@ class TrainerModule:
 
     def __init__(
         self,
-        model_name: str,
         model_class: nn.Module,
         model_configs: Any,
         optimizer_name: str,
         optimizer_params: dict,
         x: Any,
         ckpt_dir: str = os.getcwd() + '/checkpoints',
-        max_checkpoints: int = 999,
+        max_checkpoints: int = 2,
         seed=42,
     ):
         '''
         Module for summarizing all training functionalities for classification.
 
         Inputs:
-            model_name - String of the class name, used for logging and saving
             model_class - Class implementing the neural network
             model_hparams - Hyperparameters of the model, used as input to model constructor
             optimizer_name - String of the optimizer name, supporting ['sgd', 'adam', 'adamw']
@@ -48,7 +55,6 @@ class TrainerModule:
             seed - Seed to use in the model initialization
         '''
         super().__init__()
-        self.model_name = model_name
         self.model_class = model_class
         self.model_configs = model_configs
         self.optimizer_name = optimizer_name
@@ -58,6 +64,15 @@ class TrainerModule:
         self.model = self.model_class(model_configs)
 
         self.ckpt_dir = ckpt_dir
+        
+        #orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
+        #options = ocp.CheckpointManagerOptions(
+        #    max_to_keep=max_checkpoints, create=True
+        #)
+        #self.checkpoint_manager = ocp.CheckpointManager(
+        #    self.ckpt_dir, options=options
+        #)
+
         orbax_checkpointer = orbax.checkpoint.PyTreeCheckpointer()
         options = orbax.checkpoint.CheckpointManagerOptions(
             max_to_keep=max_checkpoints, create=True
@@ -115,10 +130,11 @@ class TrainerModule:
                     mutable=['batch_stats'],
                 )
                 policy_logits, value = logits
-                policy_loss = optax.softmax_cross_entropy(policy_logits, policy_tgt).mean()
+                policy_loss = optax.softmax_cross_entropy_with_integer_labels(policy_logits, policy_tgt).mean()
 
-                value_loss = optax.l2_loss(value, value_tgt).mean()
-                loss = policy_loss + value_loss
+                value_loss = optax.l2_loss(value, value_tgt)
+                value_loss = jnp.mean(value_loss)
+                loss = 0.99 * policy_loss + 0.01 * value_loss
 
                 return loss, (new_model_state, policy_loss, value_loss)
 
@@ -147,36 +163,76 @@ class TrainerModule:
             policy_acc = np.sum(np.argmax(policy_logits, axis=1) == y_policy) 
             value_acc = np.sum((value > 0) == (y_value > 0)) 
             return policy_acc, value_acc
+            
+        def evaluate(variables, baseline, batch_size=1024):
+            """A simplified evaluation by sampling. Only for debugging. 
+            Please use MCTS and run tournaments for serious evaluation."""
+            my_player = 0
+
+            env = pgx.make('bughouse')
+            rng_key = jax.random.PRNGKey(self.seed)
+            key, subkey = jax.random.split(rng_key)
+            keys = jax.random.split(subkey, batch_size)
+            state = jax.vmap(env.init)(keys)
+
+            def body_fn(val):
+                key, state, R = val
+                my_logits, _ = self.model.apply(variables, state.observation, train=False)
+                opp_logits, _ = self.model.apply(baseline, state.observation, train=False)
+                is_my_turn = (state.current_player == my_player).reshape((-1, 1))
+                logits = jnp.where(is_my_turn, my_logits, opp_logits)
+                key, subkey = jax.random.split(key)
+                action = jax.random.categorical(subkey, logits, axis=-1)
+                keys = jax.random.split(subkey, batch_size)
+                state = jax.vmap(env.step)(state, action, keys)
+                R = R + state.rewards[jnp.arange(batch_size), my_player]
+                return (key, state, R)
+
+            _, _, R = jax.lax.while_loop(
+                lambda x: ~(x[1].terminated.all()), body_fn, (key, state, jnp.zeros(batch_size))
+            )
+            return R
 
         # jit for efficiency
+        #self.train_step = jax.jit(train_step)
         self.train_step = jax.jit(train_step)
         self.eval_step = jax.jit(eval_step)
+        self.evaluate = jax.jit(evaluate)
 
     def train_loop(self, train_files, eval_files=None, epochs=7, batch_size=1024):
+        """Training model with provided files"""
+        baseline = extract_params(self.state)
         log = {} 
+
         for epoch in range(epochs):
-            print(f'Training Epoch: {epoch}/{epochs}')
+            print(f'Training Epoch: {epoch + 1}/{epochs}')
             policy_losses, value_losses = [], []
             for file in tqdm(train_files):
                 data = np.load(file)
                 with tf.device('/CPU:0'):
-                    train_loader = tf.data.Dataset.from_tensor_slices((data['obs'], data['policy_tgt'], data['value_tgt']))
-                    train_loader = train_loader.shuffle(buffer_size=2**16).batch(batch_size)
+                    train_loader = tf.data.Dataset.from_tensor_slices((data['board_planes'], data['move_planes'], data['value_planes']))
+                    train_loader = train_loader.batch(batch_size)
                 for obs, policy_tgt, value_tgt in train_loader:
-                    self.state, policy_loss, value_loss = self.train_step(self.state, obs.numpy(), policy_tgt.numpy(), value_tgt.numpy())
+                    self.state, policy_loss, value_loss = self.train_step(self.state, obs.numpy(), policy_tgt.numpy().astype(np.int32), value_tgt.numpy())
                     policy_losses.append(policy_loss.item())
                     value_losses.append(value_loss.item())
 
             policy_loss = sum(policy_losses) / len(policy_losses)
             value_loss = sum(value_losses) / len(value_losses)
+            R = self.evaluate(extract_params(self.state), baseline)
             log.update(
                 {
-                    "train/policy_loss": policy_loss,
-                    "train/value_loss": value_loss,
+                    'train/policy_loss': policy_loss,
+                    'train/value_loss': value_loss,
+                    f'eval/vs_baseline/avg_R': R.mean().item(),
+                    f'eval/vs_baseline/win_rate': ((R == 1).sum() / R.size).item(),
+                    f'eval/vs_baseline/draw_rate': ((R == 0).sum() / R.size).item(),
+                    f'eval/vs_baseline/lose_rate': ((R == -1).sum() / R.size).item(),
                 }
             )
             print(log)
-            self.save_checkpoint(self.state, epoch=epoch)
+            wandb.log(log)
+            self.save_checkpoint(step=epoch+1)
 
     def eval_model(self, files, batch_size):
         policy_acc = 0
@@ -186,7 +242,7 @@ class TrainerModule:
             data = np.load(file)
             with tf.device('/CPU:0'):
                 val_loader = tf.data.Dataset.from_tensor_slices((data['obs'], data['policy_tgt'], data['value_tgt']))
-                val_loader = val_loader.shuffle(buffer_size=2**16).batch(batch_size)
+                val_loader = val_loader.batch(batch_size)
 
             for obs, policy_tgt, value_tgt in val_loader:
                 batch_policy_acc, batch_value_acc = self.eval_step(self.state, obs.numpy(), policy_tgt.numpy(), value_tgt.numpy())
@@ -199,16 +255,26 @@ class TrainerModule:
 
         return policy_acc, value_acc
 
-    def save_checkpoint(self, train_state: TrainState, epoch=0):
+    def save_checkpoint(self, step=0):
         # Save current model at certain training iteration
-        ckpt = {'train_state': train_state}
+        #options = ocp.CheckpointManagerOptions(
+        #    max_to_keep=2, create=True
+        #)
+        #self.checkpoint_manager = ocp.CheckpointManager(
+        #    self.ckpt_dir, options=options
+        #)
+        #self.checkpoint_manager.save(step, args=ocp.args.StandardSave(self.state))
+        #self.checkpoint_manager.wait_until_finished()
+
+        ckpt = {'train_state': self.state}
         save_args = orbax_utils.save_args_from_target(ckpt)
-        self.checkpoint_manager.save(epoch, ckpt, save_kwargs={'save_args': save_args})
+        self.checkpoint_manager.save(step, ckpt, save_kwargs={'save_args': save_args})
 
-
-    def load_checkpoint(self, checkpoint_path: str) -> TrainState:
+    def load_checkpoint(self, step=0) -> TrainState:
         # Load model. We use different checkpoint for pretrained models
-        ckpt = self.checkpoint_manager.restore(checkpoint_path)
+        #self.state = self.checkpoint_manager.restore(step, args=ocp.args.StandardRestore(self.state))
+        ckpt = self.checkpoint_manager.restore(step)
+        self.state = ckpt['train_state']
         return ckpt['train_state']
 
 if __name__ == '__main__':
@@ -216,17 +282,17 @@ if __name__ == '__main__':
     import tensorflow as tf
     import glob
 
-    trainer = TrainerModule(model_name='AZResNet', model_class=AZResnet, model_configs=AZResnetConfig(
+    wandb.init()
+
+    trainer = TrainerModule(model_class=AZResnet, model_configs=AZResnetConfig(
         num_blocks=15,
         channels=256,
         policy_channels=4, 
         value_channels=8,
         num_policy_labels=2*64*78+1, 
-    ), optimizer_name='lion', optimizer_params={'learning_rate': 0.00001}, x=jnp.ones((1024, 8, 16, 32)))
-    print(os.getcwd() + '/checkpoints')
-    trainer.save_checkpoint(trainer.state)
-    #trainer.state = trainer.load_checkpoint('20240406121656')
-    #trainer.init_optimizer()
+    ), optimizer_name='lion', optimizer_params={'learning_rate': 8e-6}, x=jnp.ones((1024, 8, 16, 32)))
+    trainer.load_checkpoint(0)
+    trainer.init_optimizer()
 
-    #files = glob.glob('data/run1/*')
-    #trainer.train_loop(files, epochs=1) 
+    files = glob.glob('data/training_data/*')
+    trainer.train_loop(files, epochs=1) 
