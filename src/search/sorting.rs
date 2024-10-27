6cbcf08a98b3b63afa3b03b2eaf7e18f5152308e
Ben <bwoo2718@gmail.com>
2024-10-27 23:16:46 +1100

    Include killer moves in sorting

--- src/search/sorting.rs
+++ src/search/sorting.rs
@@ -1,7 +1,13 @@
-use super::Search;
-use shakmaty::MoveList;
+use super::defs::SearchRefs;
+use super::{Move, Search};
+use shakmaty::{MoveList, Position};
 
-pub const MVV_LVA: [[u8; 7]; 7] = [
+const TTMOVE_SORT_VALUE: i16 = 60;
+const MVV_LVA_OFFSET: i16 = 5000;
+const KILLER_VALUE: i16 = 10;
+const PROMOTION_VALUE: i16 = 56;
+
+pub const MVV_LVA: [[i16; 7]; 7] = [
     [0, 0, 0, 0, 0, 0, 0],       // victim None, attacker None, P, N, B, R, Q, K
     [0, 15, 14, 13, 12, 11, 10], // victim P, attacker None, P, N, B, R, Q, K
     [0, 25, 24, 23, 22, 21, 20], // victim N, attacker None, P, N, B, R, Q, K
@@ -12,14 +18,57 @@ pub const MVV_LVA: [[u8; 7]; 7] = [
 ];
 
 impl Search {
-    pub fn sort_moves(moves: &mut MoveList) {
-        moves.sort_by_key(|mv| {
-            let piece = mv.role() as usize;
-            let captured = match mv.capture() {
-                Some(role) => role as usize,
-                None => 0,
-            };
-            -(MVV_LVA[captured][piece] as isize)
-        });
+    pub fn score_moves(
+        moves: &mut MoveList,
+        tt_move: &Option<Move>,
+        refs: &SearchRefs,
+    ) -> [i16; 128] {
+        let mut scores: [i16; 128] = [0; 128];
+        for (i, mv) in moves.iter().enumerate() {
+            if let Some(m) = &tt_move {
+                if mv == m {
+                    scores[i] = MVV_LVA_OFFSET + TTMOVE_SORT_VALUE;
+                    continue;
+                }
+            }
+
+            if mv.is_promotion() {
+                scores[i] = MVV_LVA_OFFSET + PROMOTION_VALUE;
+            } else if mv.is_capture() {
+                let piece = mv.role() as usize;
+                let captured = match mv.capture() {
+                    Some(role) => role as usize,
+                    None => 0,
+                };
+                scores[i] = MVV_LVA_OFFSET + MVV_LVA[captured][piece];
+            } else {
+                let ply = refs.search_info.ply as usize;
+                let mut is_killer = false;
+                if let Some(first_killer) = &refs.search_info.killer_moves1[ply] {
+                    if mv == first_killer {
+                        scores[i] = MVV_LVA_OFFSET - KILLER_VALUE;
+                        is_killer = true;
+                    }
+                }
+                if let Some(second_killer) = &refs.search_info.killer_moves2[ply] {
+                    if mv == second_killer {
+                        scores[i] = MVV_LVA_OFFSET - KILLER_VALUE;
+                        is_killer = true;
+                    }
+                }
+                if !is_killer {
+                    scores[i] = refs.search_info.history[mv.role() as usize][mv.to() as usize];
+                }
+            }
+        }
+        scores
+    }
+
+    pub fn pick_move(moves: &mut MoveList, start_index: usize, scores: [i16; 128]) {
+        for i in (start_index + 1)..moves.len() {
+            if scores[i] > scores[start_index] {
+                moves.swap(start_index, i);
+            }
+        }
     }
 }
