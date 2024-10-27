08360f5d556b0c11490efdb3493720861a401baa
Ben <bwoo2718@gmail.com>
2024-10-27 23:15:57 +1100

    Include syzygy tables in search struct

--- src/search.rs
+++ src/search.rs
@@ -1,34 +1,91 @@
 mod alpha_beta;
+mod defs;
 mod eval;
 mod iter_deep;
 mod qsearch;
 mod sorting;
-use std::time::Instant;
+use crate::engine::transposition::{SearchData, TT};
+use std::sync::{Arc, Mutex};
+use std::thread::{self, JoinHandle};
 
-use shakmaty::variant::{self, Crazyhouse};
-use shakmaty::{Chess, Position};
+use crossbeam_channel::unbounded;
+use crossbeam_channel::Sender;
 
-pub struct Search {}
+use defs::{SearchInfo, SearchParams, SearchRefs};
+use shakmaty::{Chess, Move, Position};
+use shakmaty_syzygy::Tablebase;
+
+pub struct Search {
+    handle: Option<JoinHandle<()>>,
+    sender: Option<Sender<String>>,
+}
 
 impl Search {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            handle: None,
+            sender: None,
+        }
     }
 
-    pub fn init(&mut self) {
-        let quit = false;
+    pub fn init(&mut self, mtx_pos: Arc<Mutex<Chess>>, mtx_tt: Arc<Mutex<TT<SearchData>>>) {
+        let (s, r) = unbounded::<String>();
+        let h = thread::spawn(move || {
+            let mut quit = false;
+            let mut halt = true;
 
-        let mut pos = Chess::default();
+            let mut best_move: Option<Move>;
+            while !quit {
+                let cmd = r.recv().unwrap();
+                let pos = mtx_pos.lock().unwrap();
+                let mut tt = mtx_tt.lock().unwrap();
 
-        let depth = 7;
+                match cmd.as_str() {
+                    "go" => halt = false,
+                    "stop" => halt = true,
+                    "quit" => quit = true,
+                    _ => (),
+                }
 
-        let start = Instant::now();
-        let (best_move, node_count) = Search::iterative_deepening(&mut pos, depth);
-        let duration = start.elapsed();
-        let nps = node_count as f64 / duration.as_secs_f64();
-        println!("bestmove {}", best_move.to_uci(pos.castles().mode()));
-        println!("Nodes searched: {}", node_count);
-        println!("NPS: {:.2?}", nps);
-        println!("Time taken: {:.2?}", duration);
+                if !halt && !quit {
+                    let mut search_info = SearchInfo::new();
+                    let mut search_params = SearchParams {
+                        depth: 10,
+                        search_time: 15000,
+                    };
+                    let mut tables: Tablebase<Chess> = Tablebase::new();
+                    tables.add_directory("tables/chess").unwrap();
+
+                    let mut search_refs = SearchRefs {
+                        pos: &mut pos.clone(),
+                        search_params: &mut search_params,
+                        search_info: &mut search_info,
+                        tt: &mut tt,
+                        tt_enabled: true,
+                        tb: &mut tables,
+                    };
+
+                    best_move = Some(Search::iterative_deepening(&mut search_refs));
+                    println!(
+                        "bestmove {}",
+                        best_move
+                            .clone()
+                            .expect("Reason")
+                            .to_uci(search_refs.pos.castles().mode())
+                    );
+
+                    halt = true;
+                }
+            }
+        });
+
+        self.handle = Some(h);
+        self.sender = Some(s);
+    }
+
+    pub fn send(&mut self, cmd: String) {
+        if let Some(s) = &self.sender {
+            s.send(cmd).expect("Broken channel");
+        }
     }
 }
