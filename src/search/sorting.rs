52654acb3cf5a35879a51442507168a644a5a22f
Ben <bwoo2718@gmail.com>
2024-10-29 21:57:44 +1100

    Move captures with see score < 0 to the back

--- src/search/sorting.rs
+++ src/search/sorting.rs
@@ -1,63 +1,187 @@
 use super::defs::SearchRefs;
 use super::{Move, Search};
-use shakmaty::{MoveList, Position};
+use shakmaty::{Bitboard, Color, MoveList, Position, Role};
 
-const TTMOVE_SORT_VALUE: i16 = 60;
-const MVV_LVA_OFFSET: i16 = 5000;
-const KILLER_VALUE: i16 = 10;
-const PROMOTION_VALUE: i16 = 56;
+const TTMOVE_SORT_VALUE: i16 = 3000;
+const CAPTURE_BONUS: i16 = 10000;
+const KILLER_VALUE: i16 = 1000;
+const PROMOTION_VALUE: i16 = 20000;
+const COUNTER_MOVE: i16 = 4000;
 
 pub const MVV_LVA: [[i16; 7]; 7] = [
     [0, 0, 0, 0, 0, 0, 0],       // victim None, attacker None, P, N, B, R, Q, K
-    [0, 15, 14, 13, 12, 11, 10], // victim P, attacker None, P, N, B, R, Q, K
-    [0, 25, 24, 23, 22, 21, 20], // victim N, attacker None, P, N, B, R, Q, K
-    [0, 35, 34, 33, 32, 31, 30], // victim B, attacker None, P, N, B, R, Q, K
-    [0, 45, 44, 43, 42, 41, 40], // victim R, attacker None, P, N, B, R, Q, K
-    [0, 55, 54, 53, 52, 51, 50], // victim Q, attacker None, P, N, B, R, Q, K
+    [0, 6, 5, 4, 3, 2, 1],       // victim P, attacker None, P, N, B, R, Q, K
+    [0, 12, 11, 10, 9, 8, 7],    // victim N, attacker None, P, N, B, R, Q, K
+    [0, 18, 17, 16, 15, 14, 13], // victim B, attacker None, P, N, B, R, Q, K
+    [0, 24, 23, 22, 21, 20, 19], // victim R, attacker None, P, N, B, R, Q, K
+    [0, 30, 29, 28, 27, 26, 25], // victim Q, attacker None, P, N, B, R, Q, K
     [0, 0, 0, 0, 0, 0, 0],       // victim K, attacker None, P, N, B, R, Q, K
 ];
 
+pub const SEE_VALUE: [i16; 7] = [0, 100, 325, 325, 500, 1000, 30000];
+
+pub fn see(refs: &SearchRefs, m: &Move) -> i16 {
+    let pos = &refs.pos;
+    let board = pos.board();
+
+    let mut gain: [i16; 32] = [0; 32];
+    let mut depth = 0;
+
+    let mut from_set = Bitboard::from(m.from().unwrap());
+    let mut occupied = board.occupied();
+
+    let mut target = 0;
+    let mut attacker = m.role() as usize;
+    if let Some(role) = m.capture() {
+        target = role as usize;
+    }
+    gain[depth] = SEE_VALUE[target];
+
+    let mut attackers;
+    loop {
+        depth += 1;
+        gain[depth] = SEE_VALUE[attacker] - gain[depth - 1];
+        occupied ^= from_set;
+
+        if depth & 1 == 1 {
+            attackers = board.attacks_to(m.to(), pos.turn().other(), occupied) & occupied;
+        } else {
+            attackers = board.attacks_to(m.to(), pos.turn(), occupied) & occupied;
+        }
+
+        from_set = attackers & board.pawns();
+        if from_set.any() {
+            from_set = from_set.isolate_first();
+            attacker = 1;
+            continue;
+        }
+        from_set = attackers & board.knights();
+        if from_set.any() {
+            from_set = from_set.isolate_first();
+            attacker = 2;
+            continue;
+        }
+        from_set = attackers & board.bishops();
+        if from_set.any() {
+            from_set = from_set.isolate_first();
+            attacker = 3;
+            continue;
+        }
+        from_set = attackers & board.rooks();
+        if from_set.any() {
+            from_set = from_set.isolate_first();
+            attacker = 4;
+            continue;
+        }
+        from_set = attackers & board.queens();
+        if from_set.any() {
+            from_set = from_set.isolate_first();
+            attacker = 5;
+            continue;
+        }
+        break;
+    }
+
+    depth -= 1;
+    while depth > 0 {
+        gain[depth - 1] = -std::cmp::max(-gain[depth - 1], gain[depth]);
+        depth -= 1;
+    }
+    gain[0]
+}
+
 impl Search {
+    pub fn sort_moves(moves: &mut MoveList, tt_move: &Option<Move>, refs: &SearchRefs) {
+        moves.sort_by_key(|m| {
+            if let Some(mv) = &tt_move {
+                if mv == m {
+                    return i16::MAX;
+                }
+            }
+            if m.is_promotion() {
+                PROMOTION_VALUE
+            } else if m.is_capture() {
+                let piece = m.role() as usize;
+                let captured = match m.capture() {
+                    Some(role) => role as usize,
+                    None => 0,
+                };
+                let see_value = see(refs, m);
+                let capture_score = refs.search_info.get_capture_score(m);
+                let mvv = MVV_LVA[captured][piece];
+                if see_value < 0 {
+                    return see_value + mvv;
+                }
+                return CAPTURE_BONUS + see_value + capture_score;
+            } else {
+                let ply = refs.search_info.ply as usize;
+                if let Some(first_killer) = &refs.search_info.killer_moves1[ply] {
+                    if m == first_killer {
+                        return 6000;
+                    }
+                }
+                if let Some(second_killer) = &refs.search_info.killer_moves2[ply] {
+                    if m == second_killer {
+                        return 5000;
+                    }
+                }
+                if let Some(prev_m) = &refs.search_info.prev_move[ply] {
+                    if let Some(counter) = &refs.search_info.counter_moves
+                        [prev_m.from().unwrap() as usize][prev_m.to() as usize]
+                    {
+                        if m == counter {
+                            /*println!("{:?}", refs.pos.board());
+                            println!("{:?}", prev_m);
+                            println!("{:?}", m);
+                            println!();*/
+                            return 3000;
+                        }
+                    }
+                }
+                refs.search_info.get_quiet_score(m)
+            }
+        });
+        moves.reverse();
+    }
     pub fn score_moves(
         moves: &mut MoveList,
         tt_move: &Option<Move>,
         refs: &SearchRefs,
     ) -> [i16; 128] {
         let mut scores: [i16; 128] = [0; 128];
-        for (i, mv) in moves.iter().enumerate() {
-            if let Some(m) = &tt_move {
+        for (i, m) in moves.iter().enumerate() {
+            if let Some(mv) = &tt_move {
                 if mv == m {
                     scores[i] = MVV_LVA_OFFSET + TTMOVE_SORT_VALUE;
                     continue;
                 }
             }
-
-            if mv.is_promotion() {
+            if m.is_promotion() {
                 scores[i] = MVV_LVA_OFFSET + PROMOTION_VALUE;
-            } else if mv.is_capture() {
-                let piece = mv.role() as usize;
-                let captured = match mv.capture() {
+            } else if m.is_capture() {
+                let piece = m.role() as usize;
+                let captured = match m.capture() {
                     Some(role) => role as usize,
                     None => 0,
                 };
-                scores[i] = MVV_LVA_OFFSET + MVV_LVA[captured][piece];
+                scores[i] = MVV_LVA_OFFSET + see(refs, m) + MVV_LVA[captured][piece];
             } else {
                 let ply = refs.search_info.ply as usize;
                 let mut is_killer = false;
                 if let Some(first_killer) = &refs.search_info.killer_moves1[ply] {
-                    if mv == first_killer {
+                    if m == first_killer {
                         scores[i] = MVV_LVA_OFFSET - KILLER_VALUE;
                         is_killer = true;
                     }
                 }
                 if let Some(second_killer) = &refs.search_info.killer_moves2[ply] {
-                    if mv == second_killer {
+                    if m == second_killer {
                         scores[i] = MVV_LVA_OFFSET - KILLER_VALUE;
                         is_killer = true;
                     }
                 }
                 if !is_killer {
-                    scores[i] = refs.search_info.history[mv.role() as usize][mv.to() as usize];
+                    scores[i] = refs.search_info.quiet_history[m.role() as usize][m.to() as usize];
                 }
             }
         }
