d0d385cae27001aa58d2cb5da5d0084c75e678fa
Ben <bwoo2718@gmail.com>
2024-11-01 19:31:12 +1100

    Implement SEE pruning when not in check and use tt in qsearch

--- src/search/qsearch.rs
+++ src/search/qsearch.rs
@@ -1,6 +1,9 @@
+use super::sorting::see;
 use super::{defs::SearchRefs, Search};
 use crate::search::eval;
-use shakmaty::Position;
+use crate::transposition::Bound;
+use crate::types::Score;
+use shakmaty::{Move, Position};
 
 impl Search {
     pub fn qsearch(refs: &mut SearchRefs, mut alpha: i16, beta: i16) -> i16 {
@@ -14,9 +17,31 @@ impl Search {
             refs.search_info.terminated = true;
             return 0;
         }
-
         refs.search_info.nodes += 1;
-        let mut score = eval::evaluate(refs.pos);
+
+        let ply = refs.search_info.ply as usize;
+        let mut tt_move: Option<Move> = None;
+        let hash = refs.get_hash();
+        let hit = refs.tt.read(hash.0, ply);
+        let is_check = refs.pos.is_check();
+
+        let mut best_score = -Score::INFINITY;
+        let mut best_move: Option<Move> = None;
+
+        let mut score;
+        if let Some(hit) = hit {
+            match hit.bound {
+                Bound::Exact => return hit.score,
+                Bound::Alpha if hit.score <= alpha => return alpha,
+                Bound::Beta if hit.score >= beta => return beta,
+                _ => (),
+            }
+            score = hit.score;
+            tt_move = hit.m;
+        } else {
+            score = eval::evaluate(refs.pos);
+        }
+
         if score >= beta {
             return beta;
         }
@@ -25,21 +50,35 @@ impl Search {
         }
 
         let mut captures = refs.pos.capture_moves();
-        Search::sort_moves(&mut captures, &None, &None, refs);
+        Search::sort_moves(&mut captures, &None, &tt_move, refs);
 
         for m in &captures {
+            if !is_check && see(refs, m) < 0 {
+                continue;
+            }
+
             let prev_pos = refs.pos.clone();
             refs.pos.play_unchecked(m);
             score = -Search::qsearch(refs, -beta, -alpha);
             *refs.pos = prev_pos;
 
-            if score >= beta {
-                return beta;
+            if score > best_score {
+                best_score = score;
+                best_move = Some(m.clone());
             }
-            if score > alpha {
-                alpha = score;
+
+            alpha = alpha.max(score);
+            if alpha >= beta {
+                break;
             }
         }
+
+        let bound = if best_score >= beta {
+            Bound::Beta
+        } else {
+            Bound::Alpha
+        };
+        refs.tt.write(hash.0, 0, best_score, bound, best_move, ply);
         alpha
     }
 }
