295f9be096eacbdb1417fa2440fc67c3fa5a0b75
Ben <bwoo2718@gmail.com>
2024-11-01 20:35:28 +1100

    Remove tt from qsearch

--- src/search/qsearch.rs
+++ src/search/qsearch.rs
@@ -1,9 +1,7 @@
 use super::sorting::see;
 use super::{defs::SearchRefs, Search};
 use crate::search::eval;
-use crate::transposition::Bound;
-use crate::types::Score;
-use shakmaty::{Move, Position};
+use shakmaty::Position;
 
 impl Search {
     pub fn qsearch(refs: &mut SearchRefs, mut alpha: i16, beta: i16) -> i16 {
@@ -19,38 +17,19 @@ impl Search {
         }
         refs.search_info.nodes += 1;
 
-        let ply = refs.search_info.ply as usize;
-        let mut tt_move: Option<Move> = None;
-        let hash = refs.get_hash();
-        let hit = refs.tt.read(hash.0, ply);
         let is_check = refs.pos.is_check();
-
-        let mut best_score = -Score::INFINITY;
-        let mut best_move: Option<Move> = None;
-
         let mut score;
-        if let Some(hit) = hit {
-            match hit.bound {
-                Bound::Exact => return hit.score,
-                Bound::Alpha if hit.score <= alpha => return alpha,
-                Bound::Beta if hit.score >= beta => return beta,
-                _ => (),
-            }
-            score = hit.score;
-            tt_move = hit.m;
-        } else {
-            score = eval::evaluate(refs.pos);
-        }
+        let eval = eval::evaluate(refs.pos);
 
-        if score >= beta {
+        if eval >= beta {
             return beta;
         }
-        if score > alpha {
-            alpha = score;
+        if eval > alpha {
+            alpha = eval;
         }
 
         let mut captures = refs.pos.capture_moves();
-        Search::sort_moves(&mut captures, &None, &tt_move, refs);
+        Search::sort_moves(&mut captures, &None, &None, refs);
 
         for m in &captures {
             if !is_check && see(refs, m) < 0 {
@@ -62,23 +41,12 @@ impl Search {
             score = -Search::qsearch(refs, -beta, -alpha);
             *refs.pos = prev_pos;
 
-            if score > best_score {
-                best_score = score;
-                best_move = Some(m.clone());
-            }
-
             alpha = alpha.max(score);
             if alpha >= beta {
                 break;
             }
         }
 
-        let bound = if best_score >= beta {
-            Bound::Beta
-        } else {
-            Bound::Alpha
-        };
-        refs.tt.write(hash.0, 0, best_score, bound, best_move, ply);
         alpha
     }
 }
