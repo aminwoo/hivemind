403c71b13fd4fee6285175ded0dc940db6317c27
Ben <bwoo2718@gmail.com>
2024-11-03 21:55:00 +1100

    Move three fold detection to board struct

--- src/search/defs.rs
+++ src/search/defs.rs
@@ -1,22 +1,20 @@
 use super::history::History;
+use crate::board::Board;
 use crate::transposition::TranspositionTable;
-use shakmaty::{
-    zobrist::{Zobrist64, ZobristHash},
-    EnPassantMode,
-};
-use shakmaty::{Chess, Move, Role, Square};
+use crate::types::MAX_PLY;
+use shakmaty::Move;
 use std::time::Instant;
 
 pub struct SearchInfo {
     start_time: Option<Instant>,
     pub nodes: usize,
+    pub sel_depth: usize,
     pub cp: i32,
     pub ply: i8,
     pub killers: Vec<Option<Move>>,
-    pub prev_move: Vec<Option<Move>>,
     pub terminated: bool,
     pub pv: Vec<Vec<Option<Move>>>,
-    pub pv_length: [usize; 128],
+    pub pv_length: [usize; MAX_PLY],
     pub history: History,
 }
 
@@ -25,13 +23,13 @@ impl SearchInfo {
         Self {
             start_time: None,
             nodes: 0,
+            sel_depth: 0,
             cp: 0,
             ply: 0,
-            killers: vec![None; 128],
-            prev_move: vec![None; 128],
+            killers: vec![None; MAX_PLY],
             terminated: false,
-            pv: vec![vec![None; 128]; 128],
-            pv_length: [0; 128],
+            pv: vec![vec![None; MAX_PLY]; MAX_PLY],
+            pv_length: [0; MAX_PLY],
             history: History::default(),
         }
     }
@@ -54,37 +52,9 @@ pub struct SearchParams {
 }
 
 pub struct SearchRefs<'a> {
-    pub pos: &'a mut Chess,
-    pub repetitions: &'a mut Vec<Zobrist64>,
+    pub board: &'a mut Board,
     pub search_params: &'a mut SearchParams,
     pub search_info: &'a mut SearchInfo,
     pub tt: &'a mut TranspositionTable,
     pub tt_enabled: bool,
-}
-
-impl<'a> SearchRefs<'a> {
-    pub fn three_fold(&self) -> bool {
-        let mut cnt = 0;
-        let hash = self.pos.zobrist_hash(EnPassantMode::Legal);
-        for &h in self.repetitions.iter().rev() {
-            if h == hash {
-                cnt += 1;
-                if cnt > 1 {
-                    return true;
-                }
-            }
-        }
-        false
-    }
-    pub fn incr_rep(&mut self) {
-        let hash = self.pos.zobrist_hash(EnPassantMode::Legal);
-        self.repetitions.push(hash);
-    }
-    pub fn decr_rep(&mut self) {
-        self.repetitions.pop();
-    }
-
-    pub fn get_hash(&self) -> Zobrist64 {
-        self.pos.zobrist_hash(EnPassantMode::Legal)
-    }
 }
