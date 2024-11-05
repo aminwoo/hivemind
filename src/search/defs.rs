16ab3c10028edf4ee021a9029abe5c1653f4da87
Ben <bwoo2718@gmail.com>
2024-11-05 22:09:31 +1100

    Field for parameters

--- src/search/defs.rs
+++ src/search/defs.rs
@@ -1,6 +1,7 @@
 use super::history::History;
 use crate::board::Board;
 use crate::transposition::TranspositionTable;
+use crate::types::parameters::Parameters;
 use crate::types::MAX_PLY;
 use shakmaty::Move;
 use std::time::Instant;
@@ -10,7 +11,6 @@ pub struct SearchInfo {
     pub nodes: usize,
     pub sel_depth: usize,
     pub cp: i32,
-    pub ply: i8,
     pub killers: Vec<Option<Move>>,
     pub terminated: bool,
     pub pv: Vec<Vec<Option<Move>>>,
@@ -25,7 +25,6 @@ impl SearchInfo {
             nodes: 0,
             sel_depth: 0,
             cp: 0,
-            ply: 0,
             killers: vec![None; MAX_PLY],
             terminated: false,
             pv: vec![vec![None; MAX_PLY]; MAX_PLY],
@@ -53,6 +52,7 @@ pub struct SearchParams {
 
 pub struct SearchRefs<'a> {
     pub board: &'a mut Board,
+    pub params: Parameters,
     pub search_params: &'a mut SearchParams,
     pub search_info: &'a mut SearchInfo,
     pub tt: &'a mut TranspositionTable,
