fede917035fe325f089fed2403f653046e180168
aminwoo <bwoo2718@gmail.com>
2025-02-12 22:03:47 +1100

    Replace include guard with #pragma once in Planes.h and add brief function documentation

--- src/planes.h
+++ src/planes.h
@@ -1,10 +1,18 @@
-#ifndef PLANES_H
-#define PLANES_H
+#pragma once
 
 #include "board.h"
 #include "utils.h"
 #include "Fairy-Stockfish/src/types.h"
 
-void board_to_planes(Board& board, float* inputPlanes, Stockfish::Color us, bool pass);  
-
-#endif 
+/**
+ * @brief Converts a chess board state into neural network input planes.
+ *
+ * This function maps the current board configuration into a series of float planes,
+ * which can then be used as input for machine learning models or further processing.
+ *
+ * @param board The current chess board state.
+ * @param inputPlanes Pointer to an array of floats where the resulting planes are stored.
+ * @param us The color representing the current player's perspective.
+ * @param pass A flag indicating whether to process additional input (e.g., passed moves).
+ */
+void board_to_planes(Board& board, float* inputPlanes, Stockfish::Color us, bool pass);
